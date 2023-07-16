6db3901f55f4031a36a87e1e2fcefae5f6aa8b47
pmarbibi12 <marbibipanfilo@gmail.com>
2023-07-16 15:17:34 -0500

    added notations

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -47,8 +47,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/2017-08-01<br/>"
-        f"/api/v1.0/2017-08-01/2017-08-30"
+        f"/api/v1.0/(start date)            ex: /api/v1.0/2017-08-01<br/>"
+        f"/api/v1.0/(start date)/(end date) ex: /api/v1.0/2017-08-01/2017-08-30"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -68,18 +68,27 @@ def precipitation():
     #calculate the date a year from recent_date
     year_ago = recent_date_dt - dt.timedelta(days=365)
 
-    results = session.query(Measurement.prcp,Measurement.date).order_by(Measurement.date.desc()).filter(Measurement.date<=recent_date_str).\
+    #query precipitation amount from the last date recorded up to a year before the last date recorded
+    results = session.query(Measurement.prcp,Measurement.date).\
+                order_by(Measurement.date.desc()).\
+                filter(Measurement.date<=recent_date_str).\
                 filter(Measurement.date>= year_ago).all()
     
+    #close session
     session.close()
 
+    #initiate list
     results_list = []
 
+    #append results into list
     for prcp, date in results:
-        results_dict = {}
-        results_dict[date] =  prcp
-        results_list.append(results_dict)
+        results_dict = {} #initiate dictionary
+        results_dict[date] =  prcp #setup date and precipitation value as key value pairs
+        results_list.append(results_dict) #append dictionary to list
 
+        
+
+    #return results in json format
     return jsonify(results_list)
 
 
@@ -91,13 +100,19 @@ def stations():
     #query station and station names
     station_names = session.query(Station.station, Station.name).all()
 
-    station_list = []
+    #close session
+    session.close()
 
+    #initiate list
+    station_list = {}
+
+    #append entried into list
     for station, name in station_names:
-        station_dict = {}
-        station_dict[station] = name
-        station_list.append(station_dict)
+        station_dict = {} #initiate dictionary
+        station_dict[station] = name #setup station and station name as key value pairs
+        station_list.update(station_dict) #append dictionary into list
 
+    #return results in json format
     return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
@@ -126,16 +141,19 @@ def active_station():
     #calculate the date a year from recent_date_mostAct
     year_ago_mostAct = recent_date_mostAct_dt - dt.timedelta(days=365)
 
+    #query data points
     last12_act = session.query(Measurement.tobs).\
                 order_by(Measurement.date.desc()).\
                 filter(Measurement.date <= recent_date_mostAct_str).\
                 filter(Measurement.date >= year_ago_mostAct).all()
     
+    #close session
     session.close()
 
     #convert rows to a list
     data_points = [row[0] for row in last12_act]
 
+    #return results in JSON format
     return jsonify(data_points)
     
 @app.route("/api/v1.0/<start>")
@@ -143,47 +161,59 @@ def temp_min_max_avg(start):
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
+    #query all data points from given start date
     stats = session.query(func.MIN(Measurement.tobs),
                                 func.MAX(Measurement.tobs),
                                 func.AVG(Measurement.tobs)).\
                     filter(Measurement.date >= start).all()
 
+    #close session
     session.close()
 
+    #initiate list
     stats_list = []
 
+    #append results into list
     for tobs in stats:
-        stats_dict = {}
+        stats_dict = {} # initiate dictionary
+        #Set results into dictionary key value pairs
         stats_dict["Max_temp"] = stats[0][1]
         stats_dict["Min_temp"] = stats[0][0]
         stats_dict["Avg_temp"] = stats[0][2]
-        stats_list.append(stats_dict)
+        stats_list.append(stats_dict) #append dictionary to list
 
+    #return results in JSON format
     return jsonify(stats_list)
 
 @app.route("/api/v1.0/<start>/<end>")
 def temp_min_max_avg_se(start,end):
 
-    # Create our session (link) from Python to the DB
+    #Create our session (link) from Python to the DB
     session = Session(engine)
 
+    #query all data points from given start and end date
     stats = session.query(func.MIN(Measurement.tobs),
                                 func.MAX(Measurement.tobs),
                                 func.AVG(Measurement.tobs)).\
                     filter(Measurement.date >= start).\
                     filter(Measurement.date <= end).all()
 
+    #close session
     session.close()
 
+    #initiate list
     stats_list = []
 
+    #append results into list
     for tobs in stats:
-        stats_dict = {}
+        stats_dict = {} #initiate dictionary
+        #Set results into dictionary key value pairs
         stats_dict["Max_temp"] = stats[0][1]
         stats_dict["Min_temp"] = stats[0][0]
         stats_dict["Avg_temp"] = stats[0][2]
         stats_list.append(stats_dict)
 
+    #return results in JSON format
     return jsonify(stats_list)
 
 
